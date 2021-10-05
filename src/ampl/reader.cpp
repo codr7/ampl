673c55532b632f3ee55157fcb3b5ec4796ab8dd3
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-10-05 10:56:41 +0200

    Added id reader.

--- src/ampl/reader.cpp
+++ src/ampl/reader.cpp
@@ -1,9 +1,42 @@
 #include <istream>
+#include <sstream>
 #include <vector>
+#include "ampl/forms/id.hpp"
 #include "ampl/reader.hpp"
+#include "ampl/vm.hpp"
 
 namespace ampl {
   using Reader = function<optional<Form> (istream &in, Pos &pos, VM &vm)>;
+
+  optional<Form> read_form(istream &in, Pos &pos, VM &vm) {
+    vector<Reader> readers {read_ws, read_id};
+    optional<Form> f;
+    
+    for (Reader r: readers) {
+      f = r(in, pos, vm);
+      if (f) { break; }
+    }
+
+    return f;
+  }
+
+  optional<Form>read_id(istream &in, Pos &pos, VM &vm) {
+    Pos form_pos(pos);
+    char c = 0;
+    stringstream buf;
+    
+    while (in.get(c)) {
+      if (!isgraph(c)) {
+	in.unget();
+	break;
+      }
+
+      buf << c;
+    }
+
+    if (!buf.tellp()) { return nullopt; }
+    return Form(form_pos, forms::Id(vm.sym(buf.str())));
+  }
   
   optional<Form> read_ws(istream &in, Pos &pos, VM &vm) {
     char c = 0;
@@ -29,15 +62,4 @@ namespace ampl {
     return nullopt;
   }
 
-  optional<Form> read_form(istream &in, Pos &pos, VM &vm) {
-    vector<Reader> readers {read_ws};
-    optional<Form> f;
-    
-    for (Reader r: readers) {
-      f = r(in, pos, vm);
-      if (f) { break; }
-    }
-
-    return f;
-  }
 }
--- src/ampl/reader.hpp
+++ src/ampl/reader.hpp
@@ -5,6 +5,7 @@
 #include "ampl/form.hpp"
 
 namespace ampl {
+  optional<Form>read_id(istream &in, Pos &pos, VM &vm);
   optional<Form> read_ws(istream &in, Pos &pos, VM &vm);
   optional<Form> read_form(istream &in, Pos &pos, VM &vm);
 }
